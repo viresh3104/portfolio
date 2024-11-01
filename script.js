baf7ce211c223a63dd4c1cf4560a61fdebe5503b
viresh3104 <vireshnavtake3104@gmail.com>
2024-11-01 11:07:54 +0530

    version 1.1.0

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
 const menuIcon = document.querySelector('#menu-icon');
 const navlinks = document.querySelector('.nav-links');
 
+
+// on click menu will be displayed
 menuIcon.onclick = () => {
     navlinks.classList.toggle('active');
 }
+
+// on click menu will be collapsed
+document.querySelectorAll('.nav-links a').forEach(link => {
+    link.onclick = () => {
+        navlinks.classList.remove('active');
+    }
+});
+
+
+//on click of the download cv
+const downloadCV = document.querySelector('#download-cv');
